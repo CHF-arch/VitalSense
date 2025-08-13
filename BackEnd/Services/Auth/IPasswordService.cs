eb24b8fbc75bad9e2b539bc97068447ef1b46c78
GiwrgosGln <georgios.glinias@gmail.com>
2025-08-13 21:12:46 +0300

    Remove backend project

--- BackEnd/Services/Auth/IPasswordService.cs
+++
@@ -1,5 +0,0 @@
-public interface IPasswordService
-{
-    void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
-    bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt);
-}
--- BackEnd/Services/Dashboard/IDashboardService.cs
+++
@@ -1,8 +0,0 @@
-using DietManagement.Api.Models.Dashboard;
-
-namespace DietManagement.Api.Services;
-
-public interface IDashboardService
-{
-    Task<DashboardMetricsResponse> GetMetricsAsync(Guid dieticianId, DateTime utcNow);
-}
